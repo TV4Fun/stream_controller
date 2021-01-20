27872bf9dd840ae09ccbd26a8355bfc7513d09c8
Joel Croteau <joelcroteau@joelcroteau.com>
2021-01-20 15:14:34 -0800

    Rewrite for eTape

--- stream_controller.ino
+++ stream_controller.ino
@@ -1,24 +1,37 @@
-// #define DEBUG_PINS  // Use to debug individual readings from each pin. Outputs for Serial Plotter.
-// #define DEBUG_VARS  // Use to debug full set of variables. Outputs in text for Serial Monitor. Note that each output takes about 200ms, which can change the behavior of your controller a lot.
+//#define DEBUG_PLOTTER  // Output variables for Serial Plotter
+//#define DEBUG_SENSOR  // Use to debug individual readings from each pin. Outputs for Serial Monitor.
+//#define DEBUG_VARS  // Use to debug full set of variables. Outputs in text for Serial Monitor. Note that each output takes about 200ms, which can change the behavior of your controller a lot.
 // #define DEBUG_BINARY  // Send out raw binary data at high speed for reading by a customized program. Do not use in combination with either other DEBUG variable.
 #ifdef DEBUG_BINARY
 #  define WRITE(x) Serial.write((byte*)(&x), sizeof(x))
 #endif
-const byte kBaseWaterPin = 2;
-const byte kMaxWaterPin = 13;
-const byte kShutPin = A1;
-const byte kMovePin = A0;
+const byte kShutPin = 12;  // White wire
+const byte kMovePin = 13;  // Grey wire
+const byte kDepthPin = A0;  // Orange wire from eTape
 
-// All times are in second unless otherwise specified.
+// Connect jumper from 3.3V to ARef. Connect Red wire on eTape and relay controller to 5V. Connect black wire on eTape and relay controller to GND.
+
+// All times are in seconds unless otherwise specified.
 const float kTargetWaterLevel = 9.0;
 const float kStreamLag = 35.0;  // Time from when a valve is adjusted to when we expect to see a change.
 const float kValveMoveTime = 3.5;  // Time to fully open or shut the valve
 const float kFullOpenFillRate = 15.0 / 45.0;
-const float kReadingAlpha = 0.99;  // Blend reading changes to reduce noise. 0 -> Always use latest reading.
-const byte kBaseWaterLevel = 1;  // Lowest water level that can be read.
-const float kBaseFillRate = -0.1;  // Fill rate with no water input.
+const float kReadingAlpha = 0.975;  // Blend reading changes to reduce noise. 0 -> Always use latest reading.
+const float kBaseFillRate = -0.15;  // Fill rate with no water input.
 const float kAdjDeadZone = 0.25;  // Set a minimum adjustment to avoid making a lot of tiny changes. Make it variable so we can still fine tune.
 const unsigned long kPollingIntervalMillis = 25;
+
+// Water level parameters, levels in inches, resistance in ohms.
+const float kMinWaterLevel = 1.0;  // Lowest water level that can be read.
+const float kMaxWaterLevel = 12.4; // Highest water level that can be read.
+const float kMaxLevelResistance = 400.0; // Sensor resistance at highest level.
+const float kMinLevelResistance = 2000.0; // Sensor resistance at lowest level.
+const float kRRef = 2000.0;  // Resistance of series reference sensor.
+const float kDividerVin = 5.0;  // Voltage into sensor divider.
+const float kARefVolts = 3.3;  // Analog reference voltage.
+const float kWaterLevelRange = kMaxWaterLevel - kMinWaterLevel;
+const float kInchesPerOhm = kWaterLevelRange / (kMaxLevelResistance - kMinLevelResistance);
+const byte kAnalogRef = EXTERNAL;
 
 float lastWaterLevelReading;
 float estPosition = 0.0;
@@ -29,36 +42,44 @@ enum {
   OPENING = 1
 } valveMotionState = STOPPED;
 
-#ifdef DEBUG_PINS
-void printPinOutputs() {
-    for (byte pin = kBaseWaterPin; pin <= kMaxWaterPin; ++pin) {
-      if (digitalRead(pin) == HIGH)
-        Serial.print(pin);
-      else
-        Serial.print(0);
-      Serial.print(' ');
-  }
-  Serial.print('\n');
-}
+float getSensorResistance() {
+  float reading = (float)analogRead(kDepthPin);
+#ifdef DEBUG_SENSOR
+  Serial.print("Sensor reading: ");
+  Serial.println(reading);
 #endif
+  float v_sense = reading / 1023.0 * kARefVolts;
+#ifdef DEBUG_SENSOR
+  Serial.print("Calculated voltage: ");
+  Serial.println(v_sense);
+#endif
+  return kRRef / (kDividerVin / v_sense - 1.0);
+}
 
-byte getWaterLevel() {
-  #ifdef DEBUG_PINS
-    printPinOutputs();
-  #endif
-  byte waterLevel = kBaseWaterLevel;
-  for (byte pin = kBaseWaterPin; pin <= kMaxWaterPin; ++pin) {
-    if (digitalRead(pin) == LOW)
-      ++waterLevel;
-  }
-
-  return waterLevel;
+float getWaterLevel() {
+  float r_sense = getSensorResistance();
+#ifdef DEBUG_SENSOR
+  Serial.print("Calculated sensor resistance: ");
+  Serial.println(r_sense);
+  float reading = (r_sense - kMinLevelResistance) * kInchesPerOhm + kMinWaterLevel;
+  Serial.print("Calculated water level: ");
+  Serial.println(reading);
+#endif
+  
+  return (r_sense - kMinLevelResistance) * kInchesPerOhm + kMinWaterLevel;
 }
 
 float getDampenedWaterLevel(float deltaT) {
-  byte waterLevel = getWaterLevel();
+  float waterLevel = getWaterLevel();
   float alpha = pow(kReadingAlpha, deltaT);
-  float waterLevelReading = (1.0 - alpha) * (float)waterLevel + alpha * lastWaterLevelReading;
+  float waterLevelReading = (1.0 - alpha) * waterLevel + alpha * lastWaterLevelReading;
+#ifdef DEBUG_PLOTTER
+  Serial.print(waterLevel);
+  Serial.print(' ');
+  Serial.print(waterLevelReading);
+  Serial.print(' ');
+#endif
+  
 #ifdef DEBUG_VARS
   Serial.print("waterLevel: ");
   Serial.println(waterLevel);
@@ -128,24 +149,26 @@ void moveValve(float seconds, float deltaT) {
   WRITE(toMove);
   WRITE(estPosition);
 #endif
+#ifdef DEBUG_PLOTTER
+  Serial.println(toMove);
+#endif
 }
 
 void setup() {
+  analogReference(kAnalogRef);
+
   pinMode(kShutPin, OUTPUT);
   pinMode(kMovePin, OUTPUT);
   digitalWrite(kMovePin, LOW);
-
-  for (byte pin = kBaseWaterPin; pin <= kMaxWaterPin; ++pin)
-    pinMode(pin, INPUT_PULLUP);
     
-#if defined(DEBUG_VARS) || defined(DEBUG_PINS)
+#if defined(DEBUG_VARS) || defined(DEBUG_SENSOR)
   Serial.begin(9600);
   Serial.println("Started.");
+#elif defined(DEBUG_PLOTTER)
+  Serial.begin(9600);
+  Serial.println("Level Dampened d d2 activation toMove");
 #elif defined(DEBUG_BINARY)
   Serial.begin(115200);
-#endif
-#ifdef DEBUG_PINS
-  printPinOutputs();
 #endif
 
   lastWaterLevelReading = getWaterLevel();
@@ -182,6 +205,14 @@ float getServoGain(float error, float deltaT) {
   WRITE(d);
   WRITE(d2);
   WRITE(activation);
+#endif
+#ifdef DEBUG_PLOTTER
+  Serial.print(d);
+  Serial.print(' ');
+  Serial.print(d2);
+  Serial.print(' ');
+  Serial.print(activation);
+  Serial.print(' ');
 #endif
 
   return activation;
